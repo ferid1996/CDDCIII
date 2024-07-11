52839603dce24b36fe63b1ada7abc33ff96e5da5
Ferid <rzazadeferid72@gmail.com>
2024-07-11 15:41:58 +0400

    init3

--- DemoCICD/Controllers/WeatherForecastController.cs
+++ DemoCICD/Controllers/WeatherForecastController.cs
@@ -23,11 +23,11 @@ namespace DemoCICD.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(index).Date.Date.Date.Date.,
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-            .ToArray(...;
+            .ToArray();
         }
     }
 }
